117b606213f908e359bd948d11305390720765d1
JhoniSanchez <jhonimanuel7184.js@gmail.com>
2024-03-14 05:58:48 -0400

    Lettcode

--- e.js
+++ e.js
@@ -760,13 +760,43 @@
 
 
 
-a = -121
+// a = -121
 
-var isPalindrome = function (x) {
-    const b = x.toString()
-    const c = Array.from(b).toReversed().join("")
-    return b == c ? true : false
-    // console.log(b == c ? true : false)
+// var isPalindrome = function (x) {
+//     const b = x.toString()
+//     const c = Array.from(b).toReversed().join("")
+//     return b == c ? true : false
+//     // console.log(b == c ? true : false)
+// };
+
+// isPalindrome(a)
+
+
+
+
+
+const d = "MCMXCIV"
+const roman = {
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500,
+    "M":1000
+}
+var romanToInt = function(s) {
+    let sum = 0
+    for (let i = 0; i < s.length; i++) {
+        if(roman[s[i]] < roman[s[i+1]]  ){
+            sum -= roman[s[i]]
+        }  
+        else{
+            sum += roman[s[i]]
+        }      
+    }
+    console.log(sum)
+    return sum
 };
 
-isPalindrome(a)
+romanToInt(d)
